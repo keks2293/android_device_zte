4c2c7672de4a79b4e28f7b666977b12ace1a3961
rusik2293 <rusik2293@gmail.com>
2016-05-21 00:10:28 +0700

    Add files via upload

--- rootdir1/etc/tp_node.sh
+++ rootdir1/etc/tp_node.sh
@@ -31,7 +31,7 @@ fi
 
 if [ -e "/sys/bus/i2c/devices/5-005d/keypad_enable" ]; then
 	chown system:system /sys/bus/i2c/devices/5-005d/keypad_enable
-    ln -s /sys/bus/i2c/devices/5-005d/keypad_enable /data/tp/keypad_enable
+	ln -s /sys/bus/i2c/devices/5-005d/keypad_enable /data/tp/keypad_enable
 fi
 
 # ==============================================================================
@@ -73,7 +73,8 @@ if [ -e "/sys/bus/i2c/devices/5-0024/hall_mode" ]; then
 	ln -s /sys/bus/i2c/devices/5-0024/hall_mode /data/tp/hall_mode
 fi
 
-if [ -e "/sys/bus/i2c/devices/2-0024/keypad_enable" ]; then
-	chown system:system /sys/bus/i2c/devices/2-0024/keypad_enable
-	ln -s /sys/bus/i2c/devices/2-0024/keypad_enable /data/tp/keypad_enable
+if [ -e "/sys/bus/i2c/devices/5-0024/keypad_enable" ]; then
+	chown system:system /sys/bus/i2c/devices/5-0024/keypad_enable
+	ln -s /sys/bus/i2c/devices/5-0024/keypad_enable /data/tp/keypad_enable
 fi
+
