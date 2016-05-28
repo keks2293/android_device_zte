bb351fe0707090dc51f221a86846e20bbd3421bf
Atul Bansode <abansode@outlook.com>
2016-05-28 15:52:05 +0200

    TWRP changes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -212,7 +212,8 @@ TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 #TW_USE_TOOLBOX := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
-
+TW_THEME := portrait_hdpi
+TARGET_USERIMAGES_USE_F2FS := true
 TARGET_REQUIRES_SYNCHRONOUS_SETSURFACE := true
 
 # Video
