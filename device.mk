d5a888f1edf91bbdafc3a543653dcb401a87543f
rusik2293 <rusik2293@gmail.com>
2016-08-18 05:40:39 +0000

    fix

--- device.mk
+++ device.mk
@@ -363,5 +363,5 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     camera2.portability.force_api=1
 
-$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
