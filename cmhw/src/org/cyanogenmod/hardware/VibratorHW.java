c14d0cb30109941be100de3ad5af5d381d3ded4b
rusik2293 <rusik2293@gmail.com>
2016-12-01 23:46:43 +0700

    cmhw: Use cmsdk FileUtils in VibratorHW

--- cmhw/src/org/cyanogenmod/hardware/VibratorHW.java
+++ cmhw/src/org/cyanogenmod/hardware/VibratorHW.java
@@ -16,7 +16,8 @@
 
 package org.cyanogenmod.hardware;
 
-import org.cyanogenmod.hardware.util.FileUtils;
+import org.cyanogenmod.internal.util.FileUtils;
+
 import java.io.File;
 
 public class VibratorHW {
