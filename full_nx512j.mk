f6907b3d24062fcfc64c3118c69000d542341ad7
rusik2293 <rusik2293@gmail.com>
2016-12-10 01:24:34 +0700

    its 64 bit only

--- full_nx512j.mk
+++ full_nx512j.mk
@@ -15,9 +15,7 @@
 #
 
 # Inherit from those products. Most specific first.
-ifneq ($(NX512J_32_BIT),true)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-endif
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from nx512j device
